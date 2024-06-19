12cf200e0941a47d8d6f302f0a0058a2d26737df
DavidAdam1323 <davidadamgtr@gmail.com>
2024-06-19 01:16:49 +0100

    Retrieve html elements for hours, minutes and seconds using document.getElementByI().

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Digital Clock Project's Logic;
+
+// Retrieve references to HTML elements for hours, minutes, and seconds; ✅
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
